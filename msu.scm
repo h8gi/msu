7db2daad21ea42a00cddb3976b04e87f7982f7f7
hiroki yagi <hiro.yagi0245@gmail.com>
2016-02-08 22:07:49 +0900

    hello

--- msu.scm
+++ msu.scm
@@ -1,6 +1,7 @@
 ;;; msu.scm
 (module msu
     *
-  (import scheme chicken posix extras irregex data-structures ports srfi-1 srfi-69)
+  (import scheme chicken posix extras irregex data-structures ports srfi-1 srfi-69
+          utf8-srfi-13 srfi-13)
    (include "main.scm")
   )
