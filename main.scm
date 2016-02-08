f1d9a5df38b47cdbb78d8432fd326f121b2caf5f
hiroki yagi <hiro.yagi0245@gmail.com>
2016-02-08 22:06:07 +0900

    range

--- main.scm
+++ main.scm
@@ -1,13 +1,14 @@
-;;; "hello ${(+ 3 5)}" => "hello 5"
-;;; expand-string
-(define (es str)
-  (irregex-replace/all
-   '(: "$" "{" ($ (*? any)) "}") str
-   (lambda (m)
-     (with-input-from-string (irregex-match-substring m 1)
-       (lambda ()
-         (let ([val (eval (read))])
-           (if (eof-object? val) "" (->string val))))))))
+(use utf8 srfi-1)
+(define (fill-string str limit #!key (fill #\0) (right? #f))
+  (let ([dif (- limit (string-length str))])
+    (if (>= dif 0)
+        (if right?
+            (string-append str (make-string dif fill))
+            (string-append (make-string dif fill) str))
+        (if right?
+            (string-take str limit)
+            (string-drop str (abs dif))))))
+
 
 ;;; read and process
 (define (call-with-lines proc #!optional (port (current-input-port)))
@@ -31,3 +32,59 @@
 (define (mean lst)
   (let-values ([(s n m) (sum-n-mean lst)])
     m))
+;;; python's range
+(define range
+  (let ([range-inner
+         (lambda (start stop step)
+           (define (inner count acc)
+             (cond [(< count stop)
+                    (inner (+ step count) (cons count acc))]
+                   [else (reverse! acc)]))
+           (inner start '()))])
+    (case-lambda
+      [(stop) (range-inner 0 stop 1)]
+      [(start stop) (range-inner start stop 1)]
+      [(start stop step) (range-inner start stop step)])))
+
+;;; hash table
+(define (ht-ref ht key)
+  (hash-table-ref/default ht key #f))
+(define (set-default! ht key val)
+  (unless (hash-table-exists? ht key)
+    (hash-table-set! ht key val )))
+(define (ht-inc! ht key val)
+  (hash-table-set! ht key (+ val (ht-ref ht key))))
+(define (ht-set! ht key val)
+  (hash-table-set! ht key val))
+
+
+;;; string util
+(define (es-read #!optional (port (current-input-port)))
+  (define (inner acc exp)
+    (let ([ch (read-char)])
+      (cond 
+       [(char=? ch #\")
+        `(conc ,@(reverse! (cons exp acc)))]
+       [(char=? ch #\\)
+        (let ([next (read-char)])
+          (case next
+            [(#\n) (inner acc (conc exp #\newline))]
+            [(#\\) (inner acc (conc exp #\\))]
+            [(#\a) (inner acc (conc exp #\alarm))]
+            [(#\t) (inner acc (conc exp #\tab))]
+            [(#\r) (inner acc (conc exp #\return))]
+            [(#\b) (inner acc (conc exp #\backspace))]
+            [(#\v) (inner acc (conc exp #\vtab))]
+            [(#\f) (inner acc (conc exp #\page))]
+            [else  (inner acc (conc exp next))]))]
+       [(char=? ch #\$)
+        (inner (cons (read) (cons exp acc)) "")]
+       [else (inner acc (conc exp ch))])))
+  (with-input-from-port port
+    (lambda () (inner '() ""))))
+
+;;; syntax
+;;; "hello ${(+ 3 5)}" => "hello 5"
+(set-sharp-read-syntax!
+ #\"
+ es-read)
--- msu.scm
+++ msu.scm
@@ -1,6 +1,6 @@
 ;;; msu.scm
 (module msu
     *
-  (import scheme chicken posix extras irregex utf8 data-structures ports)
+  (import scheme chicken posix extras irregex data-structures ports srfi-1 srfi-69)
    (include "main.scm")
   )
